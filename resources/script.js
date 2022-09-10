78bf3c2cd61b97822065c1814e446436b273bafe
tisumaki <84583619+tisumaki@users.noreply.github.com>
2022-09-10 21:27:49 +0300

    Updated css, added a show more button

--- resources/script.js
+++ resources/script.js
@@ -9,3 +9,19 @@ $("document").ready(function($){
         }
     });
   });
+
+
+$(document).ready(function() {
+    $("button").click(function() {
+        $(".more").slideToggle(200);
+        if($(this).text()=='Show more'){
+            $(this).text('Show less');
+            $(".more").css('display', '');
+        } else {
+            $('html,body').animate({
+                scrollTop: $("#about").offset().top -50},
+                200);
+            $(this).text('Show more');
+        }
+    })
+});
